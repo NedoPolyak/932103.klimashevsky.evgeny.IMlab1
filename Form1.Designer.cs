071c8471e0a75d716cafa6daa48f3901e8876d59
NedoPolyak <61102390+NedoPolyak@users.noreply.github.com>
2024-03-01 15:17:02 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿using System.Drawing;
+
+namespace lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.inputPrice1 = new System.Windows.Forms.NumericUpDown();
+            this.btStart = new System.Windows.Forms.Button();
+            this.inputPrice2 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.inputDays = new System.Windows.Forms.NumericUpDown();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputPrice1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputPrice2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputDays)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(42, 12);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.Legend = "Legend1";
+            series4.Name = "Series2";
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(649, 353);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(151, 378);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "InPrice";
+            // 
+            // inputPrice1
+            // 
+            this.inputPrice1.Location = new System.Drawing.Point(192, 376);
+            this.inputPrice1.Name = "inputPrice1";
+            this.inputPrice1.Size = new System.Drawing.Size(120, 20);
+            this.inputPrice1.TabIndex = 2;
+            // 
+            // btStart
+            // 
+            this.btStart.Location = new System.Drawing.Point(607, 390);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(75, 23);
+            this.btStart.TabIndex = 3;
+            this.btStart.Text = "Start\\Stop";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // inputPrice2
+            // 
+            this.inputPrice2.Location = new System.Drawing.Point(192, 412);
+            this.inputPrice2.Name = "inputPrice2";
+            this.inputPrice2.Size = new System.Drawing.Size(120, 20);
+            this.inputPrice2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(151, 414);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "InPrice";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(336, 395);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Days";
+            // 
+            // inputDays
+            // 
+            this.inputDays.Location = new System.Drawing.Point(377, 390);
+            this.inputDays.Name = "inputDays";
+            this.inputDays.Size = new System.Drawing.Size(120, 20);
+            this.inputDays.TabIndex = 7;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.inputDays);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.inputPrice2);
+            this.Controls.Add(this.btStart);
+            this.Controls.Add(this.inputPrice1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputPrice1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputPrice2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputDays)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown inputPrice1;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.NumericUpDown inputPrice2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown inputDays;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace lab2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool isModelingStarted = false;
+        double priceCurrency1, priceCurrency2;
+        const double k = 0.02;
+        Random rnd = new Random();
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            if (!isModelingStarted)
+            {
+
+                priceCurrency1 = (double)inputPrice1.Value;
+                priceCurrency2 = (double)inputPrice2.Value;
+
+                chart1.Series[0].Points.Clear();
+                chart1.Series[1].Points.Clear();
+                chart1.Series[0].Points.AddXY(0, priceCurrency1);
+                chart1.Series[1].Points.AddXY(0, priceCurrency2);
+
+                timer1.Start();
+                isModelingStarted = true;
+            }
+            else
+            {
+                timer1.Stop();
+                isModelingStarted = false;
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            
+            int days = (int)inputDays.Value;
+            for (int i = 1; i <= days; i++)
+            {
+                priceCurrency1 *= (1 + k * (rnd.NextDouble() - 0.5));
+                priceCurrency2 *= (1 + k * (rnd.NextDouble() - 0.5));
+                chart1.Series[0].Points.AddXY(chart1.Series[0].Points.Count, priceCurrency1);
+                chart1.Series[1].Points.AddXY(chart1.Series[1].Points.Count, priceCurrency2);
+            }
+            
+        }
+    }
+}
